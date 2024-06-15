23396f7404c69a54c7a2ff0bd3bf3042eecd2fd8
jaylen curry <curryjaylen24@gmail.com>
2024-06-15 01:11:03 -0400

    alot of css

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+document.getElementById('carPopupBtn').addEventListener("click", function(){
+        document.getElementById('backPop').style.display = 'flex';
+    });
+
+document.getElementById('popupYourselfBtn').addEventListener("click", function(){
+    document.getElementById('secondPop').style.display = 'flex';
+});
+
+document.getElementById('close').addEventListener('click', function(){
+    document.querySelector('#backPop').style.display = 'none';
+});
+
+document.getElementById('closeBtn').addEventListener('click', function(){
+    document.querySelector('#secondPop').style.display = 'none';
+});
